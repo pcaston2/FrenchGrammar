0713c7f84d5f2b4a26a63b9e0420dea385dc80a6
Philippe Castonguay <philippe.castonguay@034gc.onmicrosoft.com>
2020-03-26 07:59:53 -0400

    viewmodel and controller done

---
+++ FrenchGrammar/Controllers/VerbController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FrenchGrammar.Models;
+using FrenchGrammarEngine;
+using FrenchGrammarEngine.Verbs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrenchGrammar.Controllers
+{
+    public class VerbController : Controller
+    {
+        public IActionResult Index()
+        {
+            VerbViewModel model = null;
+            do
+            {
+                try
+                {
+                    var currVerb = RandomVerbSelector.GetRandomVerb();
+                    var currOptions = RandomVerbSelector.GetRandomConjugationOption();
+                    var conjugation = currVerb.Conjugate(currOptions);
+                    model = new VerbViewModel();
+                    model.mood = currOptions.Mood.ToString();
+                    model.tense = currOptions.Tense.ToString();
+                    model.pronoun = currOptions.Pronoun.ToString();
+                    model.verb = currVerb.Infinitive;
+                    model.conjugation = conjugation;
+                } catch (NotImplementedException ex)
+                {
+
+                }
+            } while (model == null);
+            return View(VerbViewModel);
+        }
+    }
+}
---
+++ FrenchGrammar/Models/VerbViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrenchGrammar.Models
+{
+    public class VerbViewModel
+    {
+        public string mood;
+        public string tense;
+        public string pronoun;
+        public string conjugation;
+        public string verb;
+    }
+}
